bf88c6309f65fa713ed17291bf898a3b90fa3e7a
paullewis2013 <paullewis2013@gmail.com>
2021-02-18 00:04:21 -0500

    Replaced old files

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-
-
+//VUE
 new Vue({
     el: "#projectsDiv",
     data() {
@@ -9,3 +8,40 @@ new Vue({
         }
     }
 });
+
+
+//cursor animation at beginning
+function showCursor(){
+    document.getElementById("title").style.borderRight = "4px solid white";
+}
+
+function deleteCursor(){
+    document.getElementById("title").style.borderRight = "none";
+}
+
+function titleAnim(){
+    setTimeout(deleteCursor, 250);
+    setTimeout(showCursor, 500);
+    setTimeout(deleteCursor, 750);
+    setTimeout(showCursor, 1000);
+    setTimeout(deleteCursor, 1250);
+}
+
+function showCursor2(){
+    document.getElementById("name").style.borderRight = "4px solid white";
+}
+
+function deleteCursor2(){
+    document.getElementById("name").style.borderRight = "none";
+}
+
+function nameAnim(){
+    setTimeout(deleteCursor2, 250);
+    setTimeout(showCursor2, 500);
+    setTimeout(deleteCursor2, 750);
+    setTimeout(showCursor2, 1000);
+    setTimeout(deleteCursor2, 1250);
+}
+
+setTimeout(titleAnim, 1500);
+setTimeout(nameAnim, 3250);
