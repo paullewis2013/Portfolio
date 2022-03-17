222b0e8335e2f932ec6e31f8a37e2b203ef4603c
paullewis2013 <paullewis2013@gmail.com>
2022-03-17 13:40:30 -0400

    added empty json files and updated compile.js to list them

--- compile.js
+++ compile.js
@@ -1,13 +1,38 @@
 // this is a node script that will compile the html from json objects with other data like projects
 // to run this script use the command 
-// node compile.js on the command line
+// 'node compile.js'
+// on the command line
+
+// load node file system and path module
 const fs = require('fs');
+const path = require('path');
 
-let fileString = fs.readFileSync('index.html').toString();
-console.log(fileString);
 
-// first load all strings of json objects by looping through json folder
+// first access all json objects representing projects by looping through json folder
+for (const filePath of walkSync('json')) {
+  console.log(filePath);
+}
 
-// next create html snippets for each object
+
+// next delete all files in html/landing pages folder
+
+// next create html snippets for each object and write to html folder
 
 // finally store those as strings of html in a js file that can be loaded on site
+
+
+
+// utility functions
+
+// function from stackOverflow (https://stackoverflow.com/questions/41462606/get-all-files-recursively-in-directories-nodejs)
+// lists all files in directory
+function *walkSync(dir) {
+    const files = fs.readdirSync(dir, { withFileTypes: true });
+    for (const file of files) {
+        if (file.isDirectory()) {
+            yield* walkSync(path.join(dir, file.name));
+        } else {
+            yield path.join(dir, file.name);
+        }
+    }
+}
