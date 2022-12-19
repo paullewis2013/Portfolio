5d14406039922dc1d34820a9ab81445312bf3704
paullewis2013 <paullewis2013@gmail.com>
2022-12-18 22:33:03 -0500

    1.1.1: redid three.js script

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -3,99 +3,94 @@ const camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.inner
 
 canvas = document.getElementById("canvas")
 
-const renderer = new THREE.WebGLRenderer({canvas: canvas, alpha: true});
+var renderer = new THREE.WebGLRenderer({canvas: canvas, alpha: true});
 renderer.setSize( window.innerWidth, window.innerHeight );
-// document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry(1,10,1);
-const geometry2 = new THREE.BoxGeometry(7.5,1,1);
-const geometry3 = new THREE.SphereGeometry( 0.5, 10, 10 );
-const geometry4 = new THREE.SphereGeometry( 1, 12, 12 );
-const geometry5 = new THREE.SphereGeometry( 1.5, 16, 16 );
-const geometry6 = new THREE.SphereGeometry( 2, 16, 16 );
+window.addEventListener("resize", function() {
+    renderer.setSize( window.innerWidth, window.innerHeight );
+});
 
-// material for floating spheres
-const material = new THREE.MeshBasicMaterial( { color: 	0xf58b5f, wireframe: true } );
+//geometry for dice
+const geometry_d4 = new THREE.TetrahedronGeometry(2, 0);
+const geometry_d6 = new THREE.BoxGeometry(2, 2, 2); 
+const geometry_d8 = new THREE.OctahedronGeometry(2, 0);
+const geometry_d12 = new THREE.DodecahedronGeometry(2, 0); 
+const geometry_d20 = new THREE.IcosahedronGeometry(2, 0); 
 
-// "special cubes" are actually just spheres, I really should clean up this code
-var specialCubeArr = []
+// material for floating dice
+const material = new THREE.MeshPhongMaterial({
+    color: 0xf58b5f, //Face color
+    side: THREE.DoubleSide,
+    shininess: 50,
+    specular: 0xf58b5f,
+});
+// new THREE.MeshBasicMaterial( { color: 0xf58b5f, wireframe: true, opacity: 1 } );
 
-//make floating spheres
+// create dice
+var diceArr = []
 for(let i = 0; i < 20; i++){
     roll = Math.random();
 
-    if(roll>0.75){
-        specialCubeArr[i] = new THREE.Mesh( geometry3, material );
-    }else if(roll>0.5){
-        specialCubeArr[i] = new THREE.Mesh( geometry4, material );
-    }else if(roll>0.25){
-        specialCubeArr[i] = new THREE.Mesh( geometry5, material );
-    }else{
-        specialCubeArr[i] = new THREE.Mesh( geometry6, material );
+    if(roll>0.8){
+        diceArr[i] = new THREE.Mesh( geometry_d4, material );
+    }else if(roll>0.6){
+        diceArr[i] = new THREE.Mesh( geometry_d6, material );
+    }else if(roll>0.4){
+        diceArr[i] = new THREE.Mesh( geometry_d8, material );
+    }else if (roll > 0.2){
+        diceArr[i] = new THREE.Mesh( geometry_d12, material );
+    }else {
+        diceArr[i] = new THREE.Mesh( geometry_d20, material );
     }
-    // specialCubeArr[i] = new THREE.Mesh( geometry3, material );
-    specialCubeArr[i].position.x = -45 + -i * 5
-    specialCubeArr[i].position.y = 8 + Math.random() * 28;
-    specialCubeArr[i].position.z = -20 + Math.random() * 35;
 
-    scene.add( specialCubeArr[i] );
+    //arrange offscreen
+    diceArr[i].position.x = -45 + -i * 5
+    diceArr[i].position.y = 12 + Math.random() * 28;
+    diceArr[i].position.z = -20 + Math.random() * 40;
+    diceArr[i].rotation.x = Math.random();
+    diceArr[i].rotation.y = Math.random();
+
+    scene.add( diceArr[i] );
 }
-
-// const cube = new THREE.Mesh( geometry2, material );
-// scene.add( cube );
-// const cube2 = new THREE.Mesh( geometry2, material );
-// scene.add( cube2 );
-
-// cube2.position.y = -30;
 
 camera.position.z = 35;
 camera.position.y = 20;
 camera.rotation.x = 0;
 
-// const color = 0xFFFFFF;
 const skyColor =    0xdc322f; 
 const groundColor = 0x6c71c4;
 const intensity = 0.25;
-// const light3 = new THREE.AmbientLight(color, intensity);
-const light3 = new THREE.HemisphereLight(skyColor, groundColor, intensity);
-scene.add(light3);
+const light = new THREE.HemisphereLight(skyColor, groundColor, intensity);
+scene.add(light);
 
-// const light = new THREE.PointLight( 0xf58b5f, 2, 100 );
-// light.position.set( 0, 0, 5 );
-// scene.add( light );
-
-// const light2 = new THREE.PointLight( 0xf58b5f, 2, 100 );
-// light2.position.set( 0, 0, 30 );
-// scene.add( light2 );
-
-
-var cubeArr = [];
+var terrainArr = [];
 noise.seed(Math.random());
+
+// geometry for terrain
+const hexGeometry = new THREE.CylinderGeometry( 1, 1, 10, 6)
 
 for(let i = 0; i < 120; i++){
 
-    cubeArr[i] = []
+    terrainArr[i] = []
 
-    for(let j = 0; j < 28; j++){
+    for(let j = 0; j < 50; j++){
 
         let tempColor = randColor()
         let tempMat = new THREE.MeshBasicMaterial( { color: tempColor} );
 
-        if(Math.random() < 0.9){
+        if(Math.random() < 0.8){
             tempColor = 0x000000;
             tempMat = new THREE.MeshBasicMaterial( { color: tempColor} );
         }
 
-        let tempCube = new THREE.Mesh(geometry, tempMat);
-        tempCube.position.x = -60 + i
-        tempCube.position.z = -8 + j
+        let tempTile = new THREE.Mesh(hexGeometry, tempMat);
+        tempTile.position.x = -80 + (i * Math.sqrt(3)) + (j%2 * Math.sqrt(3)/2)
+        tempTile.position.z = -30 + j * 2
 
-        //add line here to set y position to perlin noise
-        // console.log(noise.simplex2())
-        tempCube.position.y =  1.5 * noise.simplex3(i, j, 0)
-        //changed this
-        cubeArr[i][j] = tempCube
-        scene.add(cubeArr[i][j])
+        tempTile.position.y =  1.5 * noise.simplex3(i, j, 0)
+
+        terrainArr[i][j] = tempTile
+        scene.add(terrainArr[i][j])
     }
 }
 
@@ -115,55 +110,44 @@ function randColor(){
     return colors[Math.floor(Math.random() * colors.length)]
 }
 
-var frameNum = 0
+var frameNum = 0    //used for keeping perlin noise consistent
 
 const animate = function () {
 
     frameNum++;
-    // requestAnimationFrame( animate );
 
-    for(let i = 0; i < specialCubeArr.length; i++){
-        specialCubeArr[i].rotation.x += 0.02;
-        specialCubeArr[i].position.x += 0.025;
+    //go through each dice
+    for(let i = 0; i < diceArr.length; i++){
 
-        if(specialCubeArr[i].position.x > 45){
-            specialCubeArr[i].position.x -= 90
+        //move to the right
+        diceArr[i].rotation.x += 0.02;
+        diceArr[i].position.x += 0.04;
+
+        //loop back around
+        if(diceArr[i].position.x > 45){
+            diceArr[i].position.x -= 90
         }
     }
 
-    // cube2.rotation.y += 0.005;
-    // cube2.position.y += 0.05;
-    // // light2.position.y += 0.05;
-
-    // if(cube.position.y > 45){
-    //     cube.position.y = -5;
-    //     // light.position.y = 15;
-    // }
-    // if(cube2.position.y > 45){
-    //     cube2.position.y = -5;
-    //     // light2.position.y = 15;
-    // }
-
-    for(let i = 0; i < cubeArr.length; i++){
-        for(let j= 0; j < cubeArr[i].length; j++){
+    for(let i = 0; i < terrainArr.length; i++){
+        for(let j= 0; j < terrainArr[i].length; j++){
 
             //update y position with random noise
-            cubeArr[i][j].position.y = -5 + 12 * noise.simplex3(i/20, j/20, frameNum/1000)
+            terrainArr[i][j].position.y = -5 + 12 * noise.simplex3(i/20, j/20, frameNum/1000)
 
             if(Math.random() < 0.0001){
                 let tempColor = randColor()
                 let tempMat = new THREE.MeshBasicMaterial( { color: tempColor} );
-                cubeArr[i][j].material = tempMat
+                terrainArr[i][j].material = tempMat
             }else if(Math.random() < 0.02){
                 let tempColor = 0xffffff
                 let tempMat = new THREE.MeshLambertMaterial( { color: tempColor} );
-                cubeArr[i][j].material = tempMat
+                terrainArr[i][j].material = tempMat
             }
         }
     }
 
     renderer.render( scene, camera );
+    requestAnimationFrame( animate );
 };
-
-// animate();
-var animateInterval = setInterval(animate, 40)
+requestAnimationFrame( animate );
